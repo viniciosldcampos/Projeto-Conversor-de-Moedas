288476b988889be4227ff25a8621e5c3919a3474
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-03-28 14:59:47 -0300

    Estrutura HTML e CSS do Projeto

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const convertbutton = document.querySelector('convert-button');
+
+
+function convertValues() {
+    console.log('Funcionando')
+}
+
+convertbutton.addEventListener('click', convertValues);
